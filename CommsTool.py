8f8dcf9410f27a1971ae829f43fbedc1783fde96
deeksharp <39920191+deeksharp@users.noreply.github.com>
2022-01-17 22:38:00 -0800

    use case 1 finished
    log 0 cases handled.  outputs approximately accurate

--- CommsTool.py
+++ CommsTool.py
@@ -28,14 +28,14 @@ class CommsTool:
 
         # Line Budget Calcs
         
-        cD.out_up_p_sc_transpower = m.log10(cD.up_p_sc_transpower)*10
-        cD.out_up_p_gs_transpower = m.log10(cD.up_p_gs_transpower)*10
-        cD.out_up_s_sc_transpower = m.log10(cD.up_s_sc_transpower)*10
-        cD.out_up_s_gs_transpower = m.log10(cD.up_s_gs_transpower)*10
-        cD.out_down_p_sc_transpower = m.log10(cD.down_p_sc_transpower)*10
-        cD.out_down_p_gs_transpower = m.log10(cD.down_p_gs_transpower)*10
-        cD.out_down_s_sc_transpower = m.log10(cD.down_s_sc_transpower)*10
-        cD.out_down_s_gs_transpower = m.log10(cD.down_s_gs_transpower)*10
+        cD.out_up_p_sc_transpower = m.log10(cD.up_p_sc_transpower)*10 if cD.up_p_sc_transpower > 0 else 0
+        cD.out_up_p_gs_transpower = m.log10(cD.up_p_gs_transpower)*10 if cD.up_p_gs_transpower > 0 else 0
+        cD.out_up_s_sc_transpower = m.log10(cD.up_s_sc_transpower)*10 if cD.up_s_sc_transpower > 0 else 0
+        cD.out_up_s_gs_transpower = m.log10(cD.up_s_gs_transpower)*10 if cD.up_s_gs_transpower > 0 else 0
+        cD.out_down_p_sc_transpower = m.log10(cD.down_p_sc_transpower)*10 if cD.down_p_sc_transpower > 0 else 0
+        cD.out_down_p_gs_transpower = m.log10(cD.down_p_gs_transpower)*10 if cD.down_p_gs_transpower > 0 else 0
+        cD.out_down_s_sc_transpower = m.log10(cD.down_s_sc_transpower)*10 if cD.down_s_sc_transpower > 0 else 0
+        cD.out_down_s_gs_transpower = m.log10(cD.down_s_gs_transpower)*10 if cD.down_s_gs_transpower > 0 else 0
         cD.out_up_p_sc_antgain = cD.up_p_sc_antgain
         cD.out_up_p_gs_antgain = cD.up_p_gs_antgain
         cD.out_up_s_sc_antgain = cD.up_s_sc_antgain
@@ -44,20 +44,20 @@ class CommsTool:
         cD.out_down_p_gs_antgain = cD.down_p_gs_antgain
         cD.out_down_s_sc_antgain = cD.down_s_sc_antgain
         cD.out_down_s_gs_antgain = cD.down_s_gs_antgain
-        cD.out_up_p_datarate = m.log10(cD.up_p_datarate)*10
-        cD.out_up_s_datarate = m.log10(cD.up_s_datarate)*10
-        cD.out_down_p_datarate = m.log10(cD.down_p_datarate)*10
-        cD.out_down_s_datarate = m.log10(cD.down_s_datarate)*10
+        cD.out_up_p_datarate = m.log10(cD.up_p_datarate)*10 if cD.up_p_datarate > 0 else 0
+        cD.out_up_s_datarate = m.log10(cD.up_s_datarate)*10 if cD.up_s_datarate > 0 else 0
+        cD.out_down_p_datarate = m.log10(cD.down_p_datarate)*10 if cD.down_p_datarate > 0 else 0
+        cD.out_down_s_datarate = m.log10(cD.down_s_datarate)*10 if cD.down_s_datarate > 0 else 0
         cD.out_up_p_EBNo = cD.up_p_EBNo
         cD.out_up_s_EBNo = cD.up_s_EBNo
         cD.out_down_p_EBNo = cD.down_p_EBNo
         cD.out_down_s_EBNo = cD.down_s_EBNo
-        cD.out_up_systemp = m.log10(cD.up_systemp)*10 #aka Noise temp
-        cD.out_down_systemp = m.log10(cD.down_systemp)*10
-        cD.out_up_p_pathloss = m.log10(4*3.14*cD.up_maxdistanceEarth*cD.up_p_frequency/300000000)*20
-        cD.out_up_s_pathloss = m.log10(4*3.14*cD.up_maxdistanceEarth*cD.up_s_frequency/300000000)*20
-        cD.out_down_p_pathloss = m.log10(4*3.14*cD.down_maxdistanceEarth*cD.down_p_frequency/300000000)*20
-        cD.out_down_s_pathloss = m.log10(4*3.14*cD.down_maxdistanceEarth*cD.down_s_frequency/300000000)*20
+        cD.out_up_systemp = m.log10(cD.up_systemp)*10 if cD.up_systemp > 0 else 0 #aka Noise temp
+        cD.out_down_systemp = m.log10(cD.down_systemp)*10 if cD.down_systemp > 0 else 0
+        cD.out_up_p_pathloss = m.log10(4*3.14*cD.up_maxdistanceEarth*cD.up_p_frequency/300000000)*20 if cD.up_maxdistanceEarth > 0 and cD.up_p_frequency > 0 else 0
+        cD.out_up_s_pathloss = m.log10(4*3.14*cD.up_maxdistanceEarth*cD.up_s_frequency/300000000)*20 if cD.up_maxdistanceEarth > 0 and cD.up_s_frequency > 0 else 0
+        cD.out_down_p_pathloss = m.log10(4*3.14*cD.down_maxdistanceEarth*cD.down_p_frequency/300000000)*20 if cD.down_maxdistanceEarth > 0 and cD.down_p_frequency > 0 else 0
+        cD.out_down_s_pathloss = m.log10(4*3.14*cD.down_maxdistanceEarth*cD.down_s_frequency/300000000)*20 if cD.down_maxdistanceEarth > 0 and cD.down_s_frequency > 0 else 0
         cD.out_up_otherloss = cD.up_rainloss + cD.up_lineloss + cD.up_otherloss
         cD.out_down_otherloss = cD.down_rainloss + cD.down_lineloss + cD.down_otherloss
         cD.out_up_p_margin = cD.out_up_p_gs_transpower + cD.out_up_p_gs_antgain + cD.out_up_p_sc_antgain + 228.6 - cD.out_up_p_EBNo - cD.out_up_p_datarate - cD.out_up_systemp - cD.out_up_p_pathloss - cD.out_up_otherloss
